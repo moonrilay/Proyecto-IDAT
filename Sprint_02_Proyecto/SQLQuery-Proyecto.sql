9f975d5962fe1d840f4479bd47879d092b69028d
moonrilay <128653964+moonrilay@users.noreply.github.com>
2023-03-22 21:13:16 -0500

    Delete SQLQuery-Proyecto.sql

--- Sprint_02_Proyecto/SQLQuery-Proyecto.sql
+++
@@ -1,9 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [COD_PRODUCTOS]
-      ,[NOMBRE]
-      ,[DESCRIPCION]
-      ,[MARCA]
-      ,[MODELO]
-      ,[STOCK]
-      ,[CATEGORIA]
-  FROM [BD_LOGIN].[dbo].[productos]
